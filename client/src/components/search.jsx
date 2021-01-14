77fbcb8621af116006047f4f570bbdef1cb92f4e
Adlene Saba <ad25400@hotmail.fr>
2021-01-14 18:56:44 +0100

    search started

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -2,19 +2,59 @@ import React, { Component } from "react";
 import Moment from "moment";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
-import "./home.css";
+import "./search.css";
 import "./animation.css";
 import { BsHeart, BsHeartFill } from "react-icons/bs";
 import { gender, orientation } from "../enum/genderOrientation.js";
+import Form from "react-bootstrap/Form";
 
-class Home extends Component {
+class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       pretender: [],
+      filtredPretender: [],
       likes: [],
       cookie: {},
       user: {},
+      gender: [
+        {
+          name: "All",
+          value: "",
+        },
+        {
+          name: "Man",
+          value: "man",
+        },
+        {
+          name: "Woman",
+          value: "woman",
+        },
+        {
+          name: "Other",
+          value: "other",
+        },
+      ],
+      orientation: [
+        {
+          name: "All",
+          value: "",
+        },
+        {
+          name: "Heterosexual",
+          value: "heterosexual",
+        },
+        {
+          name: "Homosexual",
+          value: "homosexual",
+        },
+        {
+          name: "Bisexual",
+          value: "bisexual",
+        },
+      ],
+      genderValue: "",
+      orientationValue: "",
     };
     this.onClick = this.onClick.bind(this);
   }
@@ -30,10 +70,9 @@ class Home extends Component {
       });
     fetch("/pretender/")
       .then((res) => res.json())
-      .then(async (pretender) => {
-        if (this.state.user[0]) {
-          this.setState({ pretender: await this.filterPretender(pretender) });
-        }
+      .then((pretender) => {
+        this.setState({ pretender });
+        this.setState({ filtredPretender: pretender });
       });
     fetch("/users/likes")
       .then((res) => res.json())
@@ -148,6 +187,29 @@ class Home extends Component {
     fetch("/users/popularity");
   };
 
+  handleGender = async (event) => {
+    await this.setState({ genderValue: event.target.value });
+    this.filteringPretender();
+  };
+
+  handleOrientation = async (event) => {
+    await this.setState({ orientationValue: event.target.value });
+    this.filteringPretender();
+  };
+
+  filteringPretender = () => {
+    let filter = this.state.pretender;
+    if (this.state.genderValue) {
+      filter = filter.filter((pretender) => pretender.gender === this.state.genderValue);
+    }
+    if (this.state.orientationValue) {
+      filter = filter.filter((pretender) => pretender.sexual_orientation === this.state.orientationValue);
+    }
+    this.setState({
+      filtredPretender: filter,
+    });
+  };
+
   onClick = (event, pretenderUid) => {
     event.preventDefault();
     fetch("/like/", {
@@ -183,14 +245,60 @@ class Home extends Component {
       });
   };
 
+  navBarStyle = {
+    width: "100%",
+    marginTop: "77px",
+    minHeight: "77px",
+    zIndex: 1,
+    color: "white",
+    backgroundColor: "#27262e",
+    display: "flex",
+    alignItems: "center",
+  };
+
   render() {
+    const { gender, genderValue, orientation, orientationValue } = this.state;
     Moment.locale("fr");
     return (
       <div>
+        <div style={this.navBarStyle} className="fixed-top">
+          <Form
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              flexWrap: "wrap",
+              alignItems: "center",
+              padding: "8px",
+            }}
+          >
+            <Form.Group className="select" controlId="exampleForm.SelectCustom">
+              <Form.Label>Gender</Form.Label>
+              <Form.Control as="select" onChange={this.handleGender} value={genderValue} custom>
+                {gender.map((item) => (
+                  <option key={item.value} value={item.value}>
+                    {item.name}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group className="select" controlId="exampleForm.SelectCustom">
+              <Form.Label>Sexual Orientation</Form.Label>
+              <Form.Control as="select" onChange={this.handleOrientation} value={orientationValue} custom>
+                {orientation.map((item) => (
+                  <option key={item.value} value={item.value}>
+                    {item.name}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </div>
         <div style={{ color: "red" }}></div>
-        <div className="cardContainer fade">
-          {this.state.pretender &&
-            this.state.pretender.map((pretender) => (
+        <div className="cardContainerSearch fade">
+          {this.state.filtredPretender &&
+            this.state.filtredPretender.map((pretender) => (
               <Link key={pretender.id} to={"profile/?uid=" + pretender.uid}>
                 <Card className="item" key={pretender.id}>
                   <Card.Img className="myPic" variant="top" src={process.env.PUBLIC_URL + pretender.path} />
@@ -243,4 +351,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default Search;
